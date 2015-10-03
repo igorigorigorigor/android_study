94aad07590bd5a2696bec591ec2a1b3b93d9d9ed
Igor Andrianov <andrianov2406@gmail.com>
2015-10-03 22:34:13 +0300

    5/ Building a Dynamic UI with Fragments - Creating a Fragment

---
+++ MyFirstApp/app/src/main/java/andrianovco/myfirstapp/ArticleFragment.java
@@ -0,0 +1,17 @@
+package andrianovco.myfirstapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ArticleFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.article_view, container, false);
+    }
+}
---
+++ MyFirstApp/app/src/main/java/andrianovco/myfirstapp/HeadlinesFragment.java
@@ -0,0 +1,21 @@
+package andrianovco.myfirstapp;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+    public class HeadlinesFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.article_view, container, false);
+        }
+    }
